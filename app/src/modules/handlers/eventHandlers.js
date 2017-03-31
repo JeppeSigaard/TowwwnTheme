63cf6e4cd6f843a5e355cfe0b965c9291835d7bb
JeppeSigaard <jeppe@smatmonkey.dk>
2017-03-31 19:41:27 +0200

    * Sorta fixed header on mobile devices * Fixed share & buy ticket buttons

--- app/src/modules/handlers/eventHandlers.js
+++ app/src/modules/handlers/eventHandlers.js
@@ -23,13 +23,15 @@ class ExtraEventHandlers {
         /* -------------------------------------------- */
         // HEADER STUFF START
         
-        let bp = _('#header').height() + _('#header').offset().top;
-        if ( window.pageYOffset >= bp ) {
-            _('#menu-show-btns').addClass('show').removeClass('active');
-            _('#site-header').addClass('fixed').removeClass('active');
-        } else {
-            _('#menu-show-btns').removeClass('show').removeClass('active');
-            _('#site-header').removeClass('fixed').removeClass('active');
+        if ( !_('body').hasClass('mobile') ) {
+            let bp = _('#header').height() + _('#header').offset().top;
+            if ( window.pageYOffset >= bp ) {
+                _('#menu-show-btns').addClass('show').removeClass('active');
+                _('#site-header').addClass('fixed').removeClass('active');
+            } else {
+                _('#menu-show-btns').removeClass('show').removeClass('active');
+                _('#site-header').removeClass('fixed').removeClass('active');
+            }
         }
         
         // HEADER STUFF END
