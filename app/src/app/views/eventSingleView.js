e1e5025ead0db250d1939bdb424bc8f43e39e54c
Jeppe Sigaard <jeppesigaard@Jeppes-MacBook-Pro.local>
2017-06-08 10:00:55 +0200

    hearts removed from events temporarily

--- app/src/app/views/eventSingleView.js
+++ app/src/app/views/eventSingleView.js
@@ -205,7 +205,7 @@ class EventSingleView extends React.Component {
         let elem = this.props.event;
         return (
             <section className="container-section" id="event-single-view">
-                <ViewTopBar icon="#icon-star" viewBox="0 0 32 32" standard={ true } heart={ true } heartFunc={ this.heart.bind(this) } clickable={ true } title={ elem != null ? elem.parentname : 'Indlæser..' } closeviewstate={ this.state.closeviewstate } vref={ this.state.vref } willChangeView={ this.willChangeView.bind(this) } name={ this.props.name } />
+                <ViewTopBar icon="#icon-star" viewBox="0 0 32 32" standard={ true } heart={ false } heartFunc={ this.heart.bind(this) } clickable={ true } title={ elem != null ? elem.parentname : 'Indlæser..' } closeviewstate={ this.state.closeviewstate } vref={ this.state.vref } willChangeView={ this.willChangeView.bind(this) } name={ this.props.name } />
 
                 <div className="scroll-container">
                     <div className="content">
